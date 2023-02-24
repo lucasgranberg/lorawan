63256b95b222973bec499fdc1319ad7de8039a38
Lucas Granberg <lukkeg@gmail.com>
2023-02-24 13:10:44 +0200

    split downlink and uplink commands

--- src/mac/mac_1_0_4/encoding/maccommandcreator.rs
+++ src/mac/mac_1_0_4/encoding/maccommandcreator.rs
@@ -1,108 +1,136 @@
 use crate::encoding::{maccommandcreator::*, maccommands::*};
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum MacCommandCreator {
+pub enum UplinkMacCommandCreator {
     LinkCheckReq(LinkCheckReqCreator),
-    LinkCheckAns(LinkCheckAnsCreator),
-    LinkADRReq(LinkADRReqCreator),
     LinkADRAns(LinkADRAnsCreator),
-    DutyCycleReq(DutyCycleReqCreator),
     DutyCycleAns(DutyCycleAnsCreator),
-    RXParamSetupReq(RXParamSetupReqCreator),
     RXParamSetupAns(RXParamSetupAnsCreator),
-    DevStatusReq(DevStatusReqCreator),
     DevStatusAns(DevStatusAnsCreator),
-    NewChannelReq(NewChannelReqCreator),
     NewChannelAns(NewChannelAnsCreator),
-    RXTimingSetupReq(RXTimingSetupReqCreator),
     RXTimingSetupAns(RXTimingSetupAnsCreator),
-    TXParamSetupReq(TXParamSetupReqCreator),
     TXParamSetupAns(TXParamSetupAnsCreator),
-    DlChannelReq(DlChannelReqCreator),
     DlChannelAns(DlChannelAnsCreator),
     DeviceTimeReq(DeviceTimeReqCreator),
+}
+#[derive(Debug, PartialEq, Eq)]
+pub enum DownlinkMacCommandCreator {
+    LinkCheckAns(LinkCheckAnsCreator),
+    LinkADRReq(LinkADRReqCreator),
+    DutyCycleReq(DutyCycleReqCreator),
+    RXParamSetupReq(RXParamSetupReqCreator),
+    DevStatusReq(DevStatusReqCreator),
+    NewChannelReq(NewChannelReqCreator),
+    RXTimingSetupReq(RXTimingSetupReqCreator),
+    TXParamSetupReq(TXParamSetupReqCreator),
+    DlChannelReq(DlChannelReqCreator),
     DeviceTimeAns(DeviceTimeAnsCreator),
 }
-
-impl MacCommandCreator {
+impl UplinkMacCommandCreator {
     #![allow(clippy::len_without_is_empty)]
     pub fn len(&self) -> usize {
         match *self {
             Self::LinkCheckReq(_) => LinkCheckReqPayload::len(),
-            Self::LinkCheckAns(_) => LinkCheckAnsPayload::len(),
-            Self::LinkADRReq(_) => LinkADRReqPayload::len(),
             Self::LinkADRAns(_) => LinkADRAnsPayload::len(),
-            Self::DutyCycleReq(_) => DutyCycleReqPayload::len(),
             Self::DutyCycleAns(_) => DutyCycleAnsPayload::len(),
-            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::len(),
             Self::RXParamSetupAns(_) => RXParamSetupAnsPayload::len(),
-            Self::DevStatusReq(_) => DevStatusReqPayload::len(),
             Self::DevStatusAns(_) => DevStatusAnsPayload::len(),
-            Self::NewChannelReq(_) => NewChannelReqPayload::len(),
             Self::NewChannelAns(_) => NewChannelAnsPayload::len(),
-            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::len(),
             Self::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::len(),
-            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::len(),
             Self::TXParamSetupAns(_) => TXParamSetupAnsPayload::len(),
-            Self::DlChannelReq(_) => DlChannelReqPayload::len(),
             Self::DlChannelAns(_) => DlChannelAnsPayload::len(),
             Self::DeviceTimeReq(_) => DeviceTimeReqPayload::len(),
-            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::len(),
         }
     }
 
     pub fn bytes(&self) -> &[u8] {
         match *self {
             Self::LinkCheckReq(_) => &[],
-            Self::LinkCheckAns(ref v) => v.build(),
-            Self::LinkADRReq(ref v) => v.build(),
             Self::LinkADRAns(ref v) => v.build(),
-            Self::DutyCycleReq(ref v) => v.build(),
             Self::DutyCycleAns(_) => &[],
-            Self::RXParamSetupReq(ref v) => v.build(),
             Self::RXParamSetupAns(ref v) => v.build(),
-            Self::DevStatusReq(_) => &[],
             Self::DevStatusAns(ref v) => v.build(),
-            Self::NewChannelReq(ref v) => v.build(),
             Self::NewChannelAns(ref v) => v.build(),
-            Self::RXTimingSetupReq(ref v) => v.build(),
             Self::RXTimingSetupAns(_) => &[],
-            Self::TXParamSetupReq(ref v) => v.build(),
             Self::TXParamSetupAns(_) => &[],
-            Self::DlChannelReq(ref v) => v.build(),
             Self::DlChannelAns(_) => &[],
             Self::DeviceTimeReq(_) => &[],
+        }
+    }
+}
+impl DownlinkMacCommandCreator {
+    #![allow(clippy::len_without_is_empty)]
+    pub fn len(&self) -> usize {
+        match *self {
+            Self::LinkCheckAns(_) => LinkCheckAnsPayload::len(),
+            Self::LinkADRReq(_) => LinkADRReqPayload::len(),
+            Self::DutyCycleReq(_) => DutyCycleReqPayload::len(),
+            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::len(),
+            Self::DevStatusReq(_) => DevStatusReqPayload::len(),
+            Self::NewChannelReq(_) => NewChannelReqPayload::len(),
+            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::len(),
+            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::len(),
+            Self::DlChannelReq(_) => DlChannelReqPayload::len(),
+            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::len(),
+        }
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        match *self {
+            Self::LinkCheckAns(ref v) => v.build(),
+            Self::LinkADRReq(ref v) => v.build(),
+            Self::DutyCycleReq(ref v) => v.build(),
+            Self::RXParamSetupReq(ref v) => v.build(),
+            Self::DevStatusReq(_) => &[],
+            Self::NewChannelReq(ref v) => v.build(),
+            Self::RXTimingSetupReq(ref v) => v.build(),
+            Self::TXParamSetupReq(ref v) => v.build(),
+            Self::DlChannelReq(ref v) => v.build(),
             Self::DeviceTimeAns(ref v) => v.build(),
         }
     }
 }
-impl SerializableMacCommand for MacCommandCreator {
+impl SerializableMacCommand for UplinkMacCommandCreator {
     fn payload_bytes(&self) -> &[u8] {
         self.bytes()
     }
 
     fn cid(&self) -> u8 {
         match *self {
-            MacCommandCreator::LinkCheckReq(_) => LinkCheckReqPayload::cid(),
-            MacCommandCreator::LinkCheckAns(_) => LinkCheckAnsPayload::cid(),
-            MacCommandCreator::LinkADRReq(_) => LinkADRReqPayload::cid(),
-            MacCommandCreator::LinkADRAns(_) => LinkADRAnsPayload::cid(),
-            MacCommandCreator::DutyCycleReq(_) => DutyCycleReqPayload::cid(),
-            MacCommandCreator::DutyCycleAns(_) => DutyCycleAnsPayload::cid(),
-            MacCommandCreator::RXParamSetupReq(_) => RXParamSetupReqPayload::cid(),
-            MacCommandCreator::RXParamSetupAns(_) => RXParamSetupAnsPayload::cid(),
-            MacCommandCreator::DevStatusReq(_) => DevStatusReqPayload::cid(),
-            MacCommandCreator::DevStatusAns(_) => DevStatusAnsPayload::cid(),
-            MacCommandCreator::NewChannelReq(_) => NewChannelReqPayload::cid(),
-            MacCommandCreator::NewChannelAns(_) => NewChannelAnsPayload::cid(),
-            MacCommandCreator::RXTimingSetupReq(_) => RXTimingSetupReqPayload::cid(),
-            MacCommandCreator::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::cid(),
-            MacCommandCreator::TXParamSetupReq(_) => TXParamSetupReqPayload::cid(),
-            MacCommandCreator::TXParamSetupAns(_) => TXParamSetupAnsPayload::cid(),
-            MacCommandCreator::DlChannelReq(_) => DlChannelReqPayload::cid(),
-            MacCommandCreator::DlChannelAns(_) => DlChannelAnsPayload::cid(),
-            MacCommandCreator::DeviceTimeReq(_) => DeviceTimeReqPayload::cid(),
-            MacCommandCreator::DeviceTimeAns(_) => DeviceTimeAnsPayload::cid(),
+            Self::LinkCheckReq(_) => LinkCheckReqPayload::cid(),
+            Self::LinkADRAns(_) => LinkADRAnsPayload::cid(),
+            Self::DutyCycleAns(_) => DutyCycleAnsPayload::cid(),
+            Self::RXParamSetupAns(_) => RXParamSetupAnsPayload::cid(),
+            Self::DevStatusAns(_) => DevStatusAnsPayload::cid(),
+            Self::NewChannelAns(_) => NewChannelAnsPayload::cid(),
+            Self::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::cid(),
+            Self::TXParamSetupAns(_) => TXParamSetupAnsPayload::cid(),
+            Self::DlChannelAns(_) => DlChannelAnsPayload::cid(),
+            Self::DeviceTimeReq(_) => DeviceTimeReqPayload::cid(),
+        }
+    }
+
+    fn payload_len(&self) -> usize {
+        self.len()
+    }
+}
+impl SerializableMacCommand for DownlinkMacCommandCreator {
+    fn payload_bytes(&self) -> &[u8] {
+        self.bytes()
+    }
+
+    fn cid(&self) -> u8 {
+        match *self {
+            Self::LinkCheckAns(_) => LinkCheckAnsPayload::cid(),
+            Self::LinkADRReq(_) => LinkADRReqPayload::cid(),
+            Self::DutyCycleReq(_) => DutyCycleReqPayload::cid(),
+            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::cid(),
+            Self::DevStatusReq(_) => DevStatusReqPayload::cid(),
+            Self::NewChannelReq(_) => NewChannelReqPayload::cid(),
+            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::cid(),
+            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::cid(),
+            Self::DlChannelReq(_) => DlChannelReqPayload::cid(),
+            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::cid(),
         }
     }
 
--- src/mac/mac_1_0_4/encoding/maccommands.rs
+++ src/mac/mac_1_0_4/encoding/maccommands.rs
@@ -11,133 +11,168 @@ use crate::encoding::maccommands::*;
 /// MacCommand represents the enumeration of all LoRaWAN MACCommands.
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug, PartialEq, Eq)]
-pub enum MacCommand<'a> {
+pub enum UplinkMacCommand<'a> {
     LinkCheckReq(LinkCheckReqPayload),
-    LinkCheckAns(LinkCheckAnsPayload<'a>),
-    LinkADRReq(LinkADRReqPayload<'a>),
     LinkADRAns(LinkADRAnsPayload<'a>),
-    DutyCycleReq(DutyCycleReqPayload<'a>),
     DutyCycleAns(DutyCycleAnsPayload),
-    RXParamSetupReq(RXParamSetupReqPayload<'a>),
     RXParamSetupAns(RXParamSetupAnsPayload<'a>),
-    DevStatusReq(DevStatusReqPayload),
     DevStatusAns(DevStatusAnsPayload<'a>),
-    NewChannelReq(NewChannelReqPayload<'a>),
     NewChannelAns(NewChannelAnsPayload<'a>),
-    RXTimingSetupReq(RXTimingSetupReqPayload<'a>),
     RXTimingSetupAns(RXTimingSetupAnsPayload),
-    TXParamSetupReq(TXParamSetupReqPayload<'a>),
     TXParamSetupAns(TXParamSetupAnsPayload),
-    DlChannelReq(DlChannelReqPayload<'a>),
     DlChannelAns(DlChannelAnsPayload),
     DeviceTimeReq(DeviceTimeReqPayload),
+}
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[derive(Debug, PartialEq, Eq)]
+pub enum DownlinkMacCommand<'a> {
+    LinkCheckAns(LinkCheckAnsPayload<'a>),
+    LinkADRReq(LinkADRReqPayload<'a>),
+    DutyCycleReq(DutyCycleReqPayload<'a>),
+    RXParamSetupReq(RXParamSetupReqPayload<'a>),
+    DevStatusReq(DevStatusReqPayload),
+    NewChannelReq(NewChannelReqPayload<'a>),
+    RXTimingSetupReq(RXTimingSetupReqPayload<'a>),
+    TXParamSetupReq(TXParamSetupReqPayload<'a>),
+    DlChannelReq(DlChannelReqPayload<'a>),
     DeviceTimeAns(DeviceTimeAnsPayload<'a>),
 }
 
-impl<'a> MacCommand<'a> {
+impl<'a> UplinkMacCommand<'a> {
     #![allow(clippy::len_without_is_empty)]
     pub fn len(&self) -> usize {
         match *self {
-            MacCommand::LinkCheckReq(_) => LinkCheckReqPayload::len(),
-            MacCommand::LinkCheckAns(_) => LinkCheckAnsPayload::len(),
-            MacCommand::LinkADRReq(_) => LinkADRReqPayload::len(),
-            MacCommand::LinkADRAns(_) => LinkADRAnsPayload::len(),
-            MacCommand::DutyCycleReq(_) => DutyCycleReqPayload::len(),
-            MacCommand::DutyCycleAns(_) => DutyCycleAnsPayload::len(),
-            MacCommand::RXParamSetupReq(_) => RXParamSetupReqPayload::len(),
-            MacCommand::RXParamSetupAns(_) => RXParamSetupAnsPayload::len(),
-            MacCommand::DevStatusReq(_) => DevStatusReqPayload::len(),
-            MacCommand::DevStatusAns(_) => DevStatusAnsPayload::len(),
-            MacCommand::NewChannelReq(_) => NewChannelReqPayload::len(),
-            MacCommand::NewChannelAns(_) => NewChannelAnsPayload::len(),
-            MacCommand::RXTimingSetupReq(_) => RXTimingSetupReqPayload::len(),
-            MacCommand::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::len(),
-            MacCommand::TXParamSetupReq(_) => TXParamSetupReqPayload::len(),
-            MacCommand::TXParamSetupAns(_) => TXParamSetupAnsPayload::len(),
-            MacCommand::DlChannelReq(_) => DlChannelReqPayload::len(),
-            MacCommand::DlChannelAns(_) => DlChannelAnsPayload::len(),
-            MacCommand::DeviceTimeReq(_) => DeviceTimeReqPayload::len(),
-            MacCommand::DeviceTimeAns(_) => DeviceTimeAnsPayload::len(),
+            Self::LinkCheckReq(_) => LinkCheckReqPayload::len(),
+            Self::LinkADRAns(_) => LinkADRAnsPayload::len(),
+            Self::DutyCycleAns(_) => DutyCycleAnsPayload::len(),
+            Self::RXParamSetupAns(_) => RXParamSetupAnsPayload::len(),
+            Self::DevStatusAns(_) => DevStatusAnsPayload::len(),
+            Self::NewChannelAns(_) => NewChannelAnsPayload::len(),
+            Self::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::len(),
+            Self::TXParamSetupAns(_) => TXParamSetupAnsPayload::len(),
+            Self::DlChannelAns(_) => DlChannelAnsPayload::len(),
+            Self::DeviceTimeReq(_) => DeviceTimeReqPayload::len(),
         }
     }
     pub fn uplink(&self) -> bool {
         match *self {
-            MacCommand::LinkCheckReq(_) => LinkCheckReqPayload::uplink(),
-            MacCommand::LinkCheckAns(_) => LinkCheckAnsPayload::uplink(),
-            MacCommand::LinkADRReq(_) => LinkADRReqPayload::uplink(),
-            MacCommand::LinkADRAns(_) => LinkADRAnsPayload::uplink(),
-            MacCommand::DutyCycleReq(_) => DutyCycleReqPayload::uplink(),
-            MacCommand::DutyCycleAns(_) => DutyCycleAnsPayload::uplink(),
-            MacCommand::RXParamSetupReq(_) => RXParamSetupReqPayload::uplink(),
-            MacCommand::RXParamSetupAns(_) => RXParamSetupAnsPayload::uplink(),
-            MacCommand::DevStatusReq(_) => DevStatusReqPayload::uplink(),
-            MacCommand::DevStatusAns(_) => DevStatusAnsPayload::uplink(),
-            MacCommand::NewChannelReq(_) => NewChannelReqPayload::uplink(),
-            MacCommand::NewChannelAns(_) => NewChannelAnsPayload::uplink(),
-            MacCommand::RXTimingSetupReq(_) => RXTimingSetupReqPayload::uplink(),
-            MacCommand::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::uplink(),
-            MacCommand::TXParamSetupReq(_) => TXParamSetupReqPayload::uplink(),
-            MacCommand::TXParamSetupAns(_) => TXParamSetupAnsPayload::uplink(),
-            MacCommand::DlChannelReq(_) => DlChannelReqPayload::uplink(),
-            MacCommand::DlChannelAns(_) => DlChannelAnsPayload::uplink(),
-            MacCommand::DeviceTimeReq(_) => DeviceTimeReqPayload::uplink(),
-            MacCommand::DeviceTimeAns(_) => DeviceTimeAnsPayload::uplink(),
+            Self::LinkCheckReq(_) => LinkCheckReqPayload::uplink(),
+            Self::LinkADRAns(_) => LinkADRAnsPayload::uplink(),
+            Self::DutyCycleAns(_) => DutyCycleAnsPayload::uplink(),
+            Self::RXParamSetupAns(_) => RXParamSetupAnsPayload::uplink(),
+            Self::DevStatusAns(_) => DevStatusAnsPayload::uplink(),
+            Self::NewChannelAns(_) => NewChannelAnsPayload::uplink(),
+            Self::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::uplink(),
+            Self::TXParamSetupAns(_) => TXParamSetupAnsPayload::uplink(),
+            Self::DlChannelAns(_) => DlChannelAnsPayload::uplink(),
+            Self::DeviceTimeReq(_) => DeviceTimeReqPayload::uplink(),
         }
     }
 
     pub fn bytes(&self) -> &[u8] {
         match *self {
-            MacCommand::LinkCheckReq(_) => &[],
-            MacCommand::LinkCheckAns(ref v) => v.0,
-            MacCommand::LinkADRReq(ref v) => v.0,
-            MacCommand::LinkADRAns(ref v) => v.0,
-            MacCommand::DutyCycleReq(ref v) => v.0,
-            MacCommand::DutyCycleAns(_) => &[],
-            MacCommand::RXParamSetupReq(ref v) => v.0,
-            MacCommand::RXParamSetupAns(ref v) => v.0,
-            MacCommand::DevStatusReq(_) => &[],
-            MacCommand::DevStatusAns(ref v) => v.0,
-            MacCommand::NewChannelReq(ref v) => v.0,
-            MacCommand::NewChannelAns(ref v) => v.0,
-            MacCommand::RXTimingSetupReq(ref v) => v.0,
-            MacCommand::RXTimingSetupAns(_) => &[],
-            MacCommand::TXParamSetupReq(ref v) => v.0,
-            MacCommand::TXParamSetupAns(_) => &[],
-            MacCommand::DlChannelReq(ref v) => v.0,
-            MacCommand::DlChannelAns(_) => &[],
-            MacCommand::DeviceTimeReq(_) => &[],
-            MacCommand::DeviceTimeAns(ref v) => v.0,
+            Self::LinkCheckReq(_) => &[],
+            Self::LinkADRAns(ref v) => v.0,
+            Self::DutyCycleAns(_) => &[],
+            Self::RXParamSetupAns(ref v) => v.0,
+            Self::DevStatusAns(ref v) => v.0,
+            Self::NewChannelAns(ref v) => v.0,
+            Self::RXTimingSetupAns(_) => &[],
+            Self::TXParamSetupAns(_) => &[],
+            Self::DlChannelAns(_) => &[],
+            Self::DeviceTimeReq(_) => &[],
+        }
+    }
+}
+impl<'a> DownlinkMacCommand<'a> {
+    #![allow(clippy::len_without_is_empty)]
+    pub fn len(&self) -> usize {
+        match *self {
+            Self::LinkCheckAns(_) => LinkCheckAnsPayload::len(),
+            Self::LinkADRReq(_) => LinkADRReqPayload::len(),
+            Self::DutyCycleReq(_) => DutyCycleReqPayload::len(),
+            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::len(),
+            Self::DevStatusReq(_) => DevStatusReqPayload::len(),
+            Self::NewChannelReq(_) => NewChannelReqPayload::len(),
+            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::len(),
+            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::len(),
+            Self::DlChannelReq(_) => DlChannelReqPayload::len(),
+            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::len(),
+        }
+    }
+    pub fn uplink(&self) -> bool {
+        match *self {
+            Self::LinkCheckAns(_) => LinkCheckAnsPayload::uplink(),
+            Self::LinkADRReq(_) => LinkADRReqPayload::uplink(),
+            Self::DutyCycleReq(_) => DutyCycleReqPayload::uplink(),
+            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::uplink(),
+            Self::DevStatusReq(_) => DevStatusReqPayload::uplink(),
+            Self::NewChannelReq(_) => NewChannelReqPayload::uplink(),
+            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::uplink(),
+            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::uplink(),
+            Self::DlChannelReq(_) => DlChannelReqPayload::uplink(),
+            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::uplink(),
+        }
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        match *self {
+            Self::LinkCheckAns(ref v) => v.0,
+            Self::LinkADRReq(ref v) => v.0,
+            Self::DutyCycleReq(ref v) => v.0,
+            Self::RXParamSetupReq(ref v) => v.0,
+            Self::DevStatusReq(_) => &[],
+            Self::NewChannelReq(ref v) => v.0,
+            Self::RXTimingSetupReq(ref v) => v.0,
+            Self::TXParamSetupReq(ref v) => v.0,
+            Self::DlChannelReq(ref v) => v.0,
+            Self::DeviceTimeAns(ref v) => v.0,
         }
     }
 }
 
-impl<'a> SerializableMacCommand for MacCommand<'a> {
+impl<'a> SerializableMacCommand for DownlinkMacCommand<'a> {
     fn payload_bytes(&self) -> &[u8] {
         self.bytes()
     }
 
     fn cid(&self) -> u8 {
         match *self {
-            MacCommand::LinkCheckReq(_) => LinkCheckReqPayload::cid(),
-            MacCommand::LinkCheckAns(_) => LinkCheckAnsPayload::cid(),
-            MacCommand::LinkADRReq(_) => LinkADRReqPayload::cid(),
-            MacCommand::LinkADRAns(_) => LinkADRAnsPayload::cid(),
-            MacCommand::DutyCycleReq(_) => DutyCycleReqPayload::cid(),
-            MacCommand::DutyCycleAns(_) => DutyCycleAnsPayload::cid(),
-            MacCommand::RXParamSetupReq(_) => RXParamSetupReqPayload::cid(),
-            MacCommand::RXParamSetupAns(_) => RXParamSetupAnsPayload::cid(),
-            MacCommand::DevStatusReq(_) => DevStatusReqPayload::cid(),
-            MacCommand::DevStatusAns(_) => DevStatusAnsPayload::cid(),
-            MacCommand::NewChannelReq(_) => NewChannelReqPayload::cid(),
-            MacCommand::NewChannelAns(_) => NewChannelAnsPayload::cid(),
-            MacCommand::RXTimingSetupReq(_) => RXTimingSetupReqPayload::cid(),
-            MacCommand::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::cid(),
-            MacCommand::TXParamSetupReq(_) => TXParamSetupReqPayload::cid(),
-            MacCommand::TXParamSetupAns(_) => TXParamSetupAnsPayload::cid(),
-            MacCommand::DlChannelReq(_) => DlChannelReqPayload::cid(),
-            MacCommand::DlChannelAns(_) => DlChannelAnsPayload::cid(),
-            MacCommand::DeviceTimeReq(_) => DeviceTimeReqPayload::cid(),
-            MacCommand::DeviceTimeAns(_) => DeviceTimeAnsPayload::cid(),
+            Self::LinkCheckAns(_) => LinkCheckAnsPayload::cid(),
+            Self::LinkADRReq(_) => LinkADRReqPayload::cid(),
+            Self::DutyCycleReq(_) => DutyCycleReqPayload::cid(),
+            Self::RXParamSetupReq(_) => RXParamSetupReqPayload::cid(),
+            Self::DevStatusReq(_) => DevStatusReqPayload::cid(),
+            Self::NewChannelReq(_) => NewChannelReqPayload::cid(),
+            Self::RXTimingSetupReq(_) => RXTimingSetupReqPayload::cid(),
+            Self::TXParamSetupReq(_) => TXParamSetupReqPayload::cid(),
+            Self::DlChannelReq(_) => DlChannelReqPayload::cid(),
+            Self::DeviceTimeAns(_) => DeviceTimeAnsPayload::cid(),
+        }
+    }
+
+    fn payload_len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<'a> SerializableMacCommand for UplinkMacCommand<'a> {
+    fn payload_bytes(&self) -> &[u8] {
+        self.bytes()
+    }
+
+    fn cid(&self) -> u8 {
+        match *self {
+            Self::LinkCheckReq(_) => LinkCheckReqPayload::cid(),
+            Self::LinkADRAns(_) => LinkADRAnsPayload::cid(),
+            Self::DutyCycleAns(_) => DutyCycleAnsPayload::cid(),
+            Self::RXParamSetupAns(_) => RXParamSetupAnsPayload::cid(),
+            Self::DevStatusAns(_) => DevStatusAnsPayload::cid(),
+            Self::NewChannelAns(_) => NewChannelAnsPayload::cid(),
+            Self::RXTimingSetupAns(_) => RXTimingSetupAnsPayload::cid(),
+            Self::TXParamSetupAns(_) => TXParamSetupAnsPayload::cid(),
+            Self::DlChannelAns(_) => DlChannelAnsPayload::cid(),
+            Self::DeviceTimeReq(_) => DeviceTimeReqPayload::cid(),
         }
     }
 
