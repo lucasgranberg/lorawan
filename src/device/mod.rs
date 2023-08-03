5f7d8cf1efe39aab45d00947c9c8d496ce2b0427
Lucas Granberg <lukkeg@gmail.com>
2023-08-03 23:29:53 +0300

    Simplify Device trait structure remove DeviceSpecs and MacDevice

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -10,7 +10,7 @@ use radio::Radio;
 use rng::Rng;
 use timer::Timer;
 
-use crate::mac::types::DR;
+use crate::mac::types::{Configuration, Credentials, Storable, DR};
 
 use self::non_volatile_store::NonVolatileStore;
 
@@ -69,14 +69,59 @@ pub trait Device {
     fn adaptive_data_rate_enabled(&self) -> bool {
         true
     }
+    /// Persist information required to maintain communication with a network server through end device power cycles.
+    fn persist_to_non_volatile(
+        &mut self,
+        configuration: &Configuration,
+        credentials: &Credentials,
+    ) -> Result<(), <<Self as Device>::NonVolatileStore as NonVolatileStore>::Error> {
+        let storable = Storable {
+            rx1_data_rate_offset: configuration.rx1_data_rate_offset,
+            rx_delay: configuration.rx_delay,
+            rx2_data_rate: configuration.rx2_data_rate,
+            rx2_frequency: configuration.rx2_frequency,
+            dev_nonce: credentials.dev_nonce,
+        };
+        if let Ok(old_storable) = self.non_volatile_store().load() {
+            if storable != old_storable {
+                trace!("overwrite {} {}", old_storable, storable);
+                self.non_volatile_store().save(storable)?;
+            } else {
+                trace!("nothing changed {}", storable);
+            }
+        } else {
+            trace!("Save fresh {}", storable);
+            self.non_volatile_store().save(storable)?;
+        }
+        Ok(())
+    }
+
+    /// Restore information required to maintain end device communication with a network server.
+    fn hydrate_from_non_volatile(
+        non_volatile_store: &mut Self::NonVolatileStore,
+        app_eui: [u8; 8],
+        dev_eui: [u8; 8],
+        app_key: [u8; 16],
+    ) -> Result<
+        (Configuration, Credentials),
+        <<Self as Device>::NonVolatileStore as NonVolatileStore>::Error,
+    > {
+        let storable: Storable = non_volatile_store.load()?;
+        let configuration = Configuration {
+            rx1_data_rate_offset: storable.rx1_data_rate_offset,
+            rx_delay: storable.rx_delay,
+            rx2_data_rate: storable.rx2_data_rate,
+            rx2_frequency: storable.rx2_frequency,
+            ..Default::default()
+        };
+        let mut credentials = Credentials::new(app_eui, dev_eui, app_key);
+        credentials.dev_nonce = storable.dev_nonce;
+        Ok((configuration, credentials))
+    }
     /// Create a DevStatusAns response to a network server specifying battery level as directed by the caller.
     fn battery_level(&self) -> Option<f32> {
         None
     }
-}
-
-/// Specification of end device-specific limits provided by the caller.
-pub trait DeviceSpecs {
     /// Get the minimum frequency supported by a device as indicated by the caller.
     fn min_frequency() -> Option<u32> {
         None
