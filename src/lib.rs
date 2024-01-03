90c9c28e5c5ae12612d425a41ef8dbab03e41dc7
Lucas Granberg <lukkeg@gmail.com>
2024-01-03 13:25:05 +0200

    remove encoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, feature(impl_trait_in_assoc_type))]
-#![allow(incomplete_features)]
+#![allow(async_fn_in_trait)]
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
